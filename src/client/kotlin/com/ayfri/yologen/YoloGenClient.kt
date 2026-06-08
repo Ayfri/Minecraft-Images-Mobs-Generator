594b9e5d2dc995a3961e88ebbaf12d664634efe9
Ayfri <pierre.ayfri@gmail.com>
2026-06-08 16:30:44 +0200

    feat: Add mod entrypoints.

---
+++ src/client/kotlin/com/ayfri/yologen/YoloGenClient.kt
@@ -0,0 +1,7 @@
+package com.ayfri.yologen
+
+import net.fabricmc.api.ClientModInitializer
+
+object YoloGenClient : ClientModInitializer {
+	override fun onInitializeClient() {}
+}
---
+++ src/main/kotlin/com/ayfri/yologen/YoloGen.kt
@@ -0,0 +1,9 @@
+package com.ayfri.yologen
+
+import net.fabricmc.api.ModInitializer
+
+object YoloGen : ModInitializer {
+	const val MOD_ID = "yologen"
+
+	override fun onInitialize() {}
+}
